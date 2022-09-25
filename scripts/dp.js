d90c698761409d105d5ab782325dbc7e61c1d292
dayanjr <dayanfrazao@byui.edu>
2022-09-24 21:21:33 -0600

    update

---
+++ scripts/dp.js
@@ -0,0 +1,7 @@
+let date = new Date();
+let year = date.getFullYear();
+
+document.querySelector('#demo').textContent = year;
+
+let currentdate = document.lastModified;
+document.querySelector('#d').textContent = currentdate;
